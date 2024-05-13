c4070c8862cfb03f729684a64b842cb581003640
Rezmii <bartoszrezmer20@gmail.com>
2024-05-13 20:31:41 +0200

    Refactor: change information section font color

--- components/InformationSection.js
+++ components/InformationSection.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
 
 const InformationSection = () => {
   // Przykładowe dane
-  const energyUsage = { electricity: '25 kWh', water: '10 m³' };
-  const weatherInfo = { temperature: '20°C', condition: 'Sunny' };
+  const energyUsage = { electricity: "25 kWh", water: "10 m³" };
+  const weatherInfo = { temperature: "20°C", condition: "Sunny" };
   const systemStatus = { online: true, errors: 2 };
 
   return (
@@ -13,17 +13,21 @@ const InformationSection = () => {
       <View style={styles.infoContainer}>
         <View style={styles.infoItem}>
           <Text style={styles.infoLabel}>Zużycie energii:</Text>
-          <Text style={styles.infoText}>{energyUsage.electricity} (prąd), {energyUsage.water} (woda)</Text>
+          <Text style={styles.infoText}>
+            {energyUsage.electricity} (prąd), {energyUsage.water} (woda)
+          </Text>
         </View>
         <View style={styles.infoItem}>
           <Text style={styles.infoLabel}>Pogoda:</Text>
-          <Text style={styles.infoText}>{weatherInfo.temperature}, {weatherInfo.condition}</Text>
+          <Text style={styles.infoText}>
+            {weatherInfo.temperature}, {weatherInfo.condition}
+          </Text>
         </View>
         <View style={styles.infoItem}>
           <Text style={styles.infoLabel}>Status systemu:</Text>
           <Text style={styles.infoText}>
-            {systemStatus.online ? 'Online' : 'Offline'}, 
-            {systemStatus.errors > 0 ? ` ${systemStatus.errors} błędy` : ''}
+            {systemStatus.online ? "Online" : "Offline"},
+            {systemStatus.errors > 0 ? ` ${systemStatus.errors} błędy` : ""}
           </Text>
         </View>
       </View>
@@ -38,13 +42,12 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 10,
-    color: 'red',
   },
   infoContainer: {
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 10,
     padding: 10,
   },
@@ -52,7 +55,7 @@ const styles = StyleSheet.create({
     marginBottom: 5,
   },
   infoLabel: {
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   infoText: {},
 });
